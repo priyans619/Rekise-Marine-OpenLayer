cd58e1d187ea3422f607598480a14fedccd05683
priyans619 <priyansh.2b@gmail.com>
2025-01-11 17:04:06 +0530

    render component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,26 @@
+import React, { useState } from "react";
+import Map from "./components/Map/Map";
+import MissionModal from "./components/Modal/MissionModal";
+import DrawButton from "./components/Button/DrawButton";
 
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleDrawClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="h-screen w-full">
+      <Map />
+      <DrawButton onClick={handleDrawClick} />
+      {isModalOpen && <MissionModal onClose={closeModal} />}
+    </div>
+  );
+};
+
+export default App;
