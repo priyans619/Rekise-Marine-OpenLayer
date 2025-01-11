acc3f4dc6e45ada2d380ebd837209a38cd04b8a7
priyans619 <priyansh.2b@gmail.com>
2025-01-11 17:03:20 +0530

    add Mission-Modal

---
+++ src/components/Modal/MissionModal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MissionModal = () => {
+  return (
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-white p-6 rounded shadow-lg w-96">
+        <h2 className="text-xl font-bold mb-4">Mission Creation</h2>
+      </div>
+    </div>
+  );
+};
+
+export default MissionModal;
