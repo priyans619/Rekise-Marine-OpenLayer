0924e33cd3f541a2d97fd09f2c16f6f496c3864d
priyans619 <priyansh.2b@gmail.com>
2025-01-11 17:25:04 +0530

    update initial modal

--- src/components/Modal/MissionModal.jsx
+++ src/components/Modal/MissionModal.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
 
 const MissionModal = ({ isOpen, onClose, onGenerateData }) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white w-96 rounded-lg shadow-lg p-6">
-      
-        <h2 className="text-lg font-semibold text-gray-800">Mission Creation</h2>
+      <div className="bg-white w-115 rounded-lg shadow-lg p-3 relative">
+        <button
+          onClick={onClose}
+          className="absolute top-3 right-2 text-gray-300 hover:text-gray-900"
+        >
+          <FaTimes size={20} />
+        </button>
 
-        <hr className="my-4 border-gray-300 shadow-sm" />
+        <h2 className="text-lg font-bold text-gray-800">Mission Creation</h2>
+
+        <hr className="my-2 border-gray-300 shadow-sm" />
 
         <h3 className="text-base font-medium text-gray-700">Waypoint Navigation</h3>
 
-        <div className="border-2 border-dotted border-gray-400 bg-gray-50 text-gray-600 text-sm p-4 my-4 rounded">
-          Click on the map to mark points of the route and press enter to complete the route.
+        <div className="border-2 border-dotted border-gray-400 bg-gray-200 text-gray-600 text-sm p-4 my-3 rounded">
+          Click on the map to mark points of the route and then press ↵ to complete the route.
         </div>
 
         <hr className="my-4 border-gray-300 shadow-sm" />
@@ -22,7 +29,7 @@ const MissionModal = ({ isOpen, onClose, onGenerateData }) => {
         <div className="flex justify-end">
           <button
             onClick={onGenerateData}
-            className="bg-blue-600 text-white px-4 py-2 rounded shadow hover:bg-blue-700"
+            className="bg-violet-500 text-white px-4 py-2 rounded shadow"
           >
             Generate Data
           </button>
