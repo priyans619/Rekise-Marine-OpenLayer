189915dd5de36a1aba3b09ad46412657ea2191b2
priyans619 <priyansh.2b@gmail.com>
2025-01-12 14:55:12 +0530

    add calculateDistance func

---
+++ src/utils/distance.js
@@ -0,0 +1,16 @@
+export const calculateDistance = (coord1, coord2) => {
+  const R = 6371e3; // Earth's radius in meters
+  const lat1 = (coord1[1] * Math.PI) / 180;
+  const lat2 = (coord2[1] * Math.PI) / 180;
+  const deltaLat = ((coord2[1] - coord1[1]) * Math.PI) / 180;
+  const deltaLon = ((coord2[0] - coord1[0]) * Math.PI) / 180;
+
+  const a =
+    Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
+    Math.cos(lat1) * Math.cos(lat2) *
+    Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c; // Distance in meters
+};
